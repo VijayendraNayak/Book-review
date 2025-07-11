9157e61c0fb9a598acf8021e0c71be445f171094
VijayendraNayak <vijayendranayak19@gmail.com>
2025-07-11 13:03:00 +0530

    final changes

--- src/main/java/com/example/book_review/dto/ReviewUpdateDTO.java
+++ src/main/java/com/example/book_review/dto/ReviewUpdateDTO.java
@@ -1,17 +1,17 @@
-//package com.example.book_review.dto;
-//
-//import jakarta.validation.constraints.NotBlank;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ReviewUpdateDTO {
-//    @NotBlank(message = "Comment cannot be blank")
-//    private String comment;
-//
-//    // Only comment can be updated
-//    // User can't change book, user, or timestamps
-//}
+package com.example.book_review.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewUpdateDTO {
+    @NotBlank(message = "Comment cannot be blank")
+    private String comment;
+
+    // Only comment can be updated
+    // User can't change book, user, or timestamps
+}
